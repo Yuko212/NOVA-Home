faaa52b226b568ad6fa53643617accca5ce33fdb
Yuko212 <102599094+Yuko212@users.noreply.github.com>
2024-09-07 22:15:15 -0300

    salvo

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Simular validação e cadastro
-    const form = document.querySelector('form');
+    const form = document.getElementById('cadastroForm');
     
     form.addEventListener('submit', function(event) {
-        event.preventDefault();
+        event.preventDefault(); // Previne o envio do formulário para o servidor
+        
+        // Simular o cadastro do usuário (pode adicionar validações extras aqui)
         alert('Cadastro realizado com sucesso!');
+        
+        // Redirecionar para a página principal
         window.location.href = 'main.html';
     });
 });
