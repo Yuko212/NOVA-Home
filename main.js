34b982e66bde70d12d27919766ecbf61094bd6d5
Yuko212 <102599094+Yuko212@users.noreply.github.com>
2024-09-11 10:21:26 -0300

    salvo

--- main.js
+++ main.js
@@ -13,3 +13,16 @@ document.addEventListener('DOMContentLoaded', function () {
         sideNav.style.width = '0'; // Fecha a barra lateral
     });
 });
+
+// Função para carregar a imagem selecionada e trocar o avatar do usuário
+function loadImage(event, imgId) {
+    const img = document.getElementById(imgId);
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    
+    reader.onload = function(e) {
+        img.src = e.target.result;
+    };
+
+    reader.readAsDataURL(file);
+}
